33adfab0f512c8a005c26ba92875a1eeda4a5450
Gustavo de Oliveira Silva <gustavosilva345.gs12@gmail.com>
2023-05-02 11:06:32 -0300

    metodos get e post

--- src/controllers/RestaurantController.js
+++ src/controllers/RestaurantController.js
@@ -14,34 +14,100 @@ export default class RestaurantController {
 
     async getRestaurants(req, res) {
         try {
-            const restaurantRef = this.db.collection('restaurant').doc('BwVLZPHypNCMyKiFiCE9')
+            const restaurantRef = this.db.collection('restaurant');
+            const snapshot = await restaurantRef.get();
+
+            if (snapshot.empty) return res.sendStatus(404);
+
+            const restaurants = [];
+
+            snapshot.forEach(doc => {
+                restaurants.push(doc.data());
+            });
+
+            const sortedRestaurants = restaurants.sort((a, b) => b.code - a.code);
+
+            res.status(200).send(sortedRestaurants);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+
+    async getRestaurantById(req, res) {
+        try {
+            const id = req.params.id;
+            const restaurantRef = this.db.collection('restaurant').doc(id);
             const doc = await restaurantRef.get();
 
             if (!doc.exists) return res.sendStatus(404);
 
             res.status(200).send(doc.data());
         } catch (error) {
-            res.status(500).send(error)
+            res.status(500).send(error);
         }
     }
 
-    getRestaurantById(req, res) {
-        const id = req.params.id;
-        res.send(`Restaurant ${id}`);
+    async getIncrementedId() {
+        try {
+            const restaurantRef = this.db.collection('restaurant').orderBy('code', 'desc').limit(1);
+            const snapshot = await restaurantRef.get();
+
+            let lastId = 0;
+
+            const lastDoc = snapshot?.docs[0];
+
+            lastId = lastDoc ? lastDoc?.data()?.code : lastId;
+
+            const newId = lastId + 1;
+
+            return newId;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
     }
 
-    updateRestaurant(req, res) {
-        const id = req.params.id;
-        res.send(`PUT restaurant ${id}`);
+    async createRestaurant(req, res) {
+        try {
+            const name = req.body.name;
+            const id = await this.getIncrementedId();
+            const restaurantRef = this.db.collection('restaurant')
+            const newRestaurant = { code: id, name };
+
+            await restaurantRef.doc(String(id)).set(newRestaurant);
+
+            res.status(200).send(`Restaurant with ID ${id} created successfully`);
+        } catch (error) {
+            console.log(error)
+            res.status(500).send(error);
+        }
     }
 
-    deleteRestaurant(req, res) {
-        const id = req.params.id;
-        res.send(`DELETE restaurant ${id}`);
+    async deleteRestaurant(req, res) {
+        try {
+            const id = req.params.id;
+            const restaurantRef = this.db.collection('restaurant').doc(id);
+
+            await restaurantRef.delete();
+
+            res.status(200).send(`Successfully deleted restaurant with ID ${id}`);
+        } catch (error) {
+            res.status(500).send(error);
+        }
     }
 
-    createRestaurant(req, res) {
-        res.send('POST restaurant');
+    async updateRestaurant(req, res) {
+        try {
+            const id = req.params.id;
+            const updateData = req.body;
+            const restaurantRef = this.db.collection('restaurant').doc(id);
+
+            await restaurantRef.update(updateData);
+
+            res.status(200).send(`Successfully updated restaurant with ID ${id}`);
+        } catch (error) {
+            res.status(500).send(error);
+        }
     }
 
     getVoteRanking(req, res) {
